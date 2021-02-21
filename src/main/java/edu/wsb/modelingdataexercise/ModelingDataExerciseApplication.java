33fc9f6b24c567c9bb31d29d8df2238372bba22d
przemyslaw wendeholm <pwenderholm@gmai.com>
2021-02-21 08:23:05 +0100

    Initial commit

---
+++ src/main/java/edu/wsb/modelingdataexercise/ModelingDataExerciseApplication.java
@@ -0,0 +1,13 @@
+package edu.wsb.modelingdataexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelingDataExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelingDataExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/wsb/modelingdataexercise/ModelingDataExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wsb.modelingdataexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelingDataExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
